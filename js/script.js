bfbadea186b03d92e5298741c17368f88b25ea2b
Nova <navidprinceofpersia@gmail.com>
2022-08-13 23:27:38 +0430

    First Commit

--- js/script.js
+++ js/script.js
@@ -1,56 +1,44 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("x");
-    span.className = "close";
-    span.appendChild(txt);
-    myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
-        var div = this.parentElement;
-        div.style.display = "none";
-    }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+function newTask() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("taskInput").value;
+    let task = document.createTextNode(inputValue);
+    li.appendChild(task);
+
     if (inputValue === '') {
-        alert("You must write something!");
+        alert("You must write something!"); 
     } else {
         document.getElementById("myUL").appendChild(li);
+        document.getElementById("taskInput").value = ""; //after click, value is empty
     }
-    document.getElementById("myInput").value = "";
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
+    let span = document.createElement("SPAN");
+    let x = document.createTextNode("×");
+    span.appendChild(x);
     li.appendChild(span);
+    span.className = "close"; //close className is in CSS
 
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function () {
-            var div = this.parentElement;
+            let div = this.parentElement;
             div.style.display = "none";
         }
     }
 }
 
+// // Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle("checked");
+    }
+}, false);
+
+// // Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+        let div = this.parentElement;
+        div.style.display = "none"
+    }
+}
